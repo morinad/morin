cbb0dcc60b2d21980047b89035b1f16423138c01
Директ-ПРО <44451631+morinad@users.noreply.github.com>
2024-11-20 12:43:00 +0300

    28

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='morin',
-    version='0.2.27',
+    version='0.2.28',
     packages=find_packages(),
     description='Помощь в подключениях и загрузке в БД',
     long_description=long_description,
